017553a1ba358857b49c4b0d02ca84ba14ee8e38
Henok Kirubel <henokhm2@gmail.com>
2023-08-14 23:18:38 +0300

    Add endpoints

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 
+const allRegionsData = require('./data/all-regions.json');
+const asiaData = require('./data/asia.json')
+const africaData = require('./data/africa.json')
+const latinAmericaData = require('./data/latin-america.json')
+const europeData = require('./data/europe.json')
+const northernAmericaData = require('./data/northern-america.json')
+const oceaniaData = require('./data/oceania.json')
+
 const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
+
+// Root endpoint returns data for all regions
+app.get('/', (req, res) => res.json(allRegionsData));
+
+// Return data for each region
+app.get('/asia', (req, res) => res.json(asiaData));
+app.get('/africa', (req, res) => res.json(africaData));
+app.get('/latin-america', (req, res) => res.json(latinAmericaData));
+app.get('/europe', (req, res) => res.json(europeData));
+app.get('/northern-america', (req, res) => res.json(northernAmericaData));
+app.get('/oceania', (req, res) => res.json(oceaniaData));
+
 
 app.get('/data1', (req, res) => {
   const data = { message: 'Hello from endpoint 1!' };
